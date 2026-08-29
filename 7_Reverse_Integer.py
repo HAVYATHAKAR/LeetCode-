0c58252cec8ede3fc1fc07ac16431c3f6e2319f6
Havya Thakar <hinadthakar@gmail.com>
2026-08-29 07:57:05 +0530

    Implement integer reversal in Solution class

---
+++ 7_Reverse_Integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        rev = int(str(x)[::-1])
+        rev *= sign
+
+        if rev < -2**31 or rev > 2**31 - 1:
+            return 0
+
+        return rev
